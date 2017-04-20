6427f939c960e6652d4e4b327859628465a0d625
catying <chenmai1102@gmail.com>
2017-04-20 20:58:06 +0800

    "只差银行交互！"

--- shop/views.py
+++ shop/views.py
@@ -10,6 +10,7 @@ from website.mixin import FrontMixin
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from shop.models import *
 
+
 # Create your views here.
 
 
@@ -84,19 +85,43 @@ class ItemUpdateView(LoginRequiredMixin, UserPassesTestMixin, FrontMixin, Update
     def get_object(self, queryset=None):
         return Item.objects.get(pk=int(self.kwargs['pk']))
 
+    def get_context_data(self, **kwargs):
+        context = super(ItemUpdateView, self).get_context_data(**kwargs)
+        context['prototype_list'] = Prototype.objects.all()
+        return context
+
+
+class ShopListView(FrontMixin, ListView):
+    template_name = 'shop/list.html'
+    model = MyUser
+    context_object_name = 'myuser_list'
+
+    def get_queryset(self):
+        return MyUser.objects.filter(level__gte=3)
+
+
+class CartView(LoginRequiredMixin, FrontMixin, ListView):
+    template_name = 'shop/cart.html'
+    model = Item
+    context_object_name = 'item_list'
+    login_url = reverse_lazy('login')
+
+    def get_queryset(self):
+        return Item.objects.filter(locking_id=self.request.user.myuser.id)
+
 
 @login_required(login_url=reverse_lazy("login"))
 def api_add_to_cart(request):
     if request.method == "POST":
-        item_id = request.POST.get("item_id", "").__int__()
+        item_id = int(request.POST.get("item_id", ""))
         myuser_id = request.user.myuser.id
         item = Item.objects.get(id=item_id)
-        if not item.locking_id:
-            return HttpResponse(json.dumps({'result': '0', 'message': '已被锁定'}), content_type='application/json')
+        if item.locking_id:
+            return HttpResponse(json.dumps({'result': 0, 'message': '已被锁定'}), content_type='application/json')
         else:
             item.locking_id = myuser_id
             item.save()
-            return HttpResponse(json.dumps({'result': '1'}), content_type='application/json')
+            return HttpResponse(json.dumps({'result': 1}), content_type='application/json')
     else:
         pass
 
@@ -104,14 +129,38 @@ def api_add_to_cart(request):
 @login_required(login_url=reverse_lazy("login"))
 def api_move_from_cart(request):
     if request.method == "POST":
-        item_id = request.POST.get("item_id", "").__int__()
+        item_id = int(request.POST.get("item_id", ""))
         myuser_id = request.user.myuser.id
         item = Item.objects.get(id=item_id)
         if item.locking_id != myuser_id:
-            return HttpResponse(json.dumps({'result': '0', 'message': '您没有锁定该宠物'}), content_type=
-                                'application/json')
+            return HttpResponse(json.dumps({'result': 0, 'message': '您没有锁定该宠物'}), content_type=
+            'application/json')
         else:
             item.locking_id = 0
             item.save()
-            return HttpResponse(json.dumps({'result': '1'}), content_type='application/json')
+            return HttpResponse(json.dumps({'result': 1}), content_type='application/json')
 
+
+@login_required(login_url=reverse_lazy("login"))
+def api_unlock(request):
+    if request.GET.get("item_id", ""):
+        item_id = int(request.GET.get("item_id", ""))
+        item = Item.objects.get(id=item_id)
+        if item.myuser == request.user.myuser:
+            item.locking_id = 0
+            item.save()
+            return HttpResponse(json.dumps({'result': 1}), content_type='application/json')
+        else:
+            return HttpResponse(json.dumps({'result': 0, 'message': '您无权解锁该宠物'}), content_type='application/json')
+
+
+@login_required(login_url=reverse_lazy("login"))
+def api_delete(request):
+    if request.method == "POST":
+        item_id = int(request.POST.get("item_id"))
+        item = Item.objects.get(id=item_id)
+        if item.myuser == request.user.myuser:
+            item.delete()
+            return HttpResponse(json.dumps({'result': 1}), content_type='application/json')
+        else:
+            return HttpResponse(json.dumps({'result': 0, 'message': '你无法删除该宠物'}), content_type='application/json')
